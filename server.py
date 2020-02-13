a264bcbb0e87b8eed54d1b7829c48c09000530dd
Debjit <debjitchatterjee31@gmail.com>
2020-02-13 19:34:47 +0530

    "Socket Programming"

---
+++ server.py
@@ -0,0 +1,20 @@
+import socket
+
+msg = "Welcome to the Server!"
+print(f'{len(msg):<10}'+msg)
+
+
+
+
+'''
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+
+while True:
+	clientsocket,address = s.accept()
+	print(f"Connection from {address} has been established!")
+	clientsocket.send(bytes("welcome to the Server!", "utf-8"))
+	clientsocket.close()
+'''
+
