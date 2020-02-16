8e01f058927874938fda92f06c96251392a55719
Debjit <debjitchatterjee31@gmail.com>
2020-02-16 16:12:13 +0530

    Server Added

--- server.py
+++ server.py
@@ -1,12 +1,7 @@
 import socket
 
-msg = "Welcome to the Server!"
-print(f'{len(msg):<10}'+msg)
-
-
-
-
-'''
+##msg = "Welcome to the Server!"
+##print(f'{len(msg):<10}'+msg)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
@@ -16,5 +11,5 @@ while True:
 	print(f"Connection from {address} has been established!")
 	clientsocket.send(bytes("welcome to the Server!", "utf-8"))
 	clientsocket.close()
-'''
+
 
